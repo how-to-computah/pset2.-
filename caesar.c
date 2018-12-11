da0db3617d54c57e51ee15beedacf9d7fc2b5b70
make-go-fast-good <45416860+make-go-fast-good@users.noreply.github.com>
2018-12-10 22:42:23 -0600

    fin.
    //error: format specifies type 'int' but the argument has type 'string' (aka 'char *')

--- caesar.c
+++ caesar.c
@@ -1,39 +1,69 @@
 #include <cs50.h>
 #include <ctype.h>
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-//#define MSG_LEN strlen(plain_text);
-#define KEY argv[argc - 1]
-//ci=(pi+k)mod26
+#define KEY atoi(argv[argc - 1])
 
 char *get_msg();
 
 int main(int argc, string argv[])
 {
     char *msg_ptr = 0;
-    int i = 0;
-    int len = 0;
+    int i, c, len = 0;
+    int k = KEY;
+    int ASCII_offset = 0;
 
+// We gots no key, get outta here!!
+    if (argv[1] == NULL)
+    {
+        exit(1);
+    }
     // Get message to be encrypted from user.
     msg_ptr = get_msg();
     len = strlen(msg_ptr);
 
-    char p_text [len];
+    printf("ciphertext: ");
 
     for (i = 0; i < len; i++)
     {
-        p_text [i] = *(msg_ptr + i);
-    }
+        // Are you a letter?
+        if (isalpha(*(msg_ptr + i)))
+        {
+            if (isupper(*(msg_ptr + i)))
+                // A = 65 ; Z = 90
+            {
+                ASCII_offset = 64;
+            }
+            else
+                // a = 97 ; z = 122
+            {
+                ASCII_offset = 96;
+            }
+            // wrap em.
+            if (((*(msg_ptr + i) - ASCII_offset) + k) > 26)
+            {
+                // c = (p + k) % 26
+                c = ((((*(msg_ptr + i) - ASCII_offset) + k) % 26) + ASCII_offset);
 
-    for (i = 0; i < len; i++)
-    {
-        printf("%c", p_text[i]);
+                printf("%c", c);
+            }
+            // just add to em.
+            else
+            {
+                printf("%c", (*(msg_ptr + i) + k));
+            }
+
+        }
+        // Guess not, we won't mess with you.
+        else
+        {
+            printf("%c", (*(msg_ptr + i)));
+        }
+
     }
 
     printf("\n");
-    printf("%d\n", argc);
 
     return 0;
 }
@@ -43,7 +73,7 @@ char *get_msg()
     static char *plain_text = "";
     int len = 0;
 
-    plain_text = get_string("plaintext: ");
+    plain_text = get_string("plaintext:  ");
 
     len = strlen(plain_text);
 
